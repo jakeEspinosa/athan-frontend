85e4e9f93a49e9ec36ee9de28bc9cb5f55d15c46
Jake Espinosa <jacob.sa.espinosa@gmail.com>
2023-05-02 15:18:40 -0700

    Add calculation drop down.

--- src/components/main/MainForm.tsx
+++ src/components/main/MainForm.tsx
@@ -1,3 +1,4 @@
+import DropDown from "./DropDown";
 import TextInput from "./TextInput";
 import { VStack } from "@chakra-ui/react";
 
@@ -19,6 +20,61 @@ const inputFields = [
   },
 ];
 
+const dropDownOptions = [
+  {
+    option: "University of Islamic Sciences, Karachi",
+    value: 1,
+  },
+  {
+    option: "Islamic Society of North America",
+    value: 2,
+  },
+  {
+    option: "Muslim World League",
+    value: 3,
+  },
+  {
+    option: "Umm Al-Qura University, Makkah",
+    value: 4,
+  },
+  {
+    option: "Egyptian General Authority of Survey",
+    value: 5,
+  },
+  {
+    option: "Institute of Geophysics, University of Tehran",
+    value: 7,
+  },
+  {
+    option: "Gulf Region",
+    value: 8,
+  },
+  {
+    option: "Kuwait",
+    value: 9,
+  },
+  {
+    option: "Qatar",
+    value: 10,
+  },
+  {
+    option: "Majlis Ugama Islam Singapura, Singapore",
+    value: 11,
+  },
+  {
+    option: "Union Organization islamic de France",
+    value: 12,
+  },
+  {
+    option: "Diyanet İşleri Başkanlığı, Turkey",
+    value: 13,
+  },
+  {
+    option: "Spiritual Administration of Muslims of Russia",
+    value: 14,
+  },
+];
+
 const MainForm = () => {
   return (
     <>
@@ -30,6 +86,12 @@ const MainForm = () => {
             helperText={field.helperText}
           />
         ))}
+
+        <DropDown
+          placeholder="Islamic Society of North America"
+          label="Calculation Method"
+          options={dropDownOptions}
+        />
       </VStack>
     </>
   );

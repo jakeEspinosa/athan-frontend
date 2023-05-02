89f41af17a559fcefb46be3ea0d39acf9bcd6017
Jake Espinosa <jacob.sa.espinosa@gmail.com>
2023-05-01 22:33:28 -0700

    Create reusable TextInput.

---
+++ src/components/TextInput.tsx
@@ -0,0 +1,26 @@
+import {
+  FormControl,
+  FormLabel,
+  Input,
+  FormHelperText,
+} from "@chakra-ui/react";
+
+interface Props {
+  placeholder: string;
+  label: string;
+  helperText?: string;
+}
+
+const TextInput = ({ placeholder, label, helperText }: Props) => {
+  return (
+    <>
+      <FormControl isRequired>
+        <FormLabel>{label}</FormLabel>
+        <Input placeholder={placeholder} />
+        <FormHelperText>{helperText}</FormHelperText>
+      </FormControl>
+    </>
+  );
+};
+
+export default TextInput;
